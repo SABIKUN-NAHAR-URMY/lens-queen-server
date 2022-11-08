5698498c0ab2d2f47da5559ec9a2ced9ca61f5bd
SABIKUN-NAHAR-URMY <snurmy18@gmail.com>
2022-11-08 19:28:16 +0600

    index file updated with mongodb

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = "mongodb+srv://<username>:<password>@cluster0.ho0d8c2.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
