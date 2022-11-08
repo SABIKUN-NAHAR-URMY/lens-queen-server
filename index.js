02220cb78778964a4d66fa4aa8dbd4fdc788edf6
SABIKUN-NAHAR-URMY <snurmy18@gmail.com>
2022-11-08 21:42:21 +0600

    servicesall id data loaded

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config();
@@ -17,11 +17,24 @@ console.log(uri);
 async function run() {
     const servicesCollection = client.db('lensQueen').collection('services');
 
-    app.get('/services', async (req, res) => {
+    app.get('/servicesAll', async (req, res) => {
         const query = {};
         const cursor = servicesCollection.find(query);
         const services = await cursor.toArray();
         res.send(services);
+    })
+    app.get('/servicesLimit', async (req, res) => {
+        const query = {};
+        const cursor = servicesCollection.find(query).limit(3);
+        const services = await cursor.toArray();
+        res.send(services);
+    })
+
+    app.get('/servicesAll/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id)};
+        const service = await servicesCollection.findOne(query);
+        res.send(service);
     })
 
 }
