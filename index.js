f2d04b9ee3fd23454cb6e87a8ccde4c462efe6bb
SABIKUN-NAHAR-URMY <snurmy18@gmail.com>
2022-11-08 19:40:03 +0600

    get services done

--- index.js
+++ index.js
@@ -11,16 +11,23 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ho0d8c2.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
+async function run() {
     const servicesCollection = client.db('lensQueen').collection('services');
-    
+
+    app.get('/services', async (req, res) => {
+        const query = {};
+        const cursor = servicesCollection.find(query);
+        const services = await cursor.toArray();
+        res.send(services);
+    })
+
 }
 run().catch(error => console.error(error));
 
 app.get('/', (req, res) => {
-  res.send('Lens-Queen server running!')
+    res.send('Lens-Queen server running!')
 })
 
 app.listen(port, () => {
-  console.log(`Lens-Queen server listening on port ${port}`)
+    console.log(`Lens-Queen server listening on port ${port}`)
 })
